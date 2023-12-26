b6021de8e1343f8b0f7f60cb6fb514a0079f96a2
Julian Finelli <julianfinelli97@gmail.com>
2023-12-26 17:03:24 -0300

    chore: modificando

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import AppRouter from './AppRouter/AppRouter';
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import { FilterProvider } from './context/FilterContext';
 import './App.css';
 
-
-function App() {
-
+function RootApp() {
   return (
     <NextUIProvider>
       <FilterProvider>
-        <AppRouter/>
+        <AppRouter />
       </FilterProvider>
     </NextUIProvider>
   );
 }
 
-export default App;
+export default RootApp;
--- src/AppRouter/AppRouter.jsx
+++ src/AppRouter/AppRouter.jsx
@@ -1,28 +1,25 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import Navbar from "../components/Navbar"
-import Presentacion from "../components/Presentacion"
-import Characters from "../components/Characters/Characters"
-import Episode from "../components/Episode/Episode"
-import Location from "../components/Location/Location"
-import CharacterPage from "../components/CharacterPage/CharacterPage"
-
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Presentacion from "../components/Presentacion";
+import Characters from "../components/Characters/Characters";
+import Episode from "../components/Episode/Episode";
+import Location from "../components/Location/Location";
+import CharacterPage from "../components/CharacterPage/CharacterPage";
 
 const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='*' element={<Navigate to={"/inicio/"} />} />
+        <Route path='/' element={<Presentacion />} />
+        <Route path='/characters' element={<Characters />} />
+        <Route path='/characters/:id' element={<CharacterPage />} />
+        <Route path='/Episode' element={<Episode />} />
+        <Route path='/Location' element={<Location />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-    return(
-        <BrowserRouter>
-            <Navbar/>
-                <Routes>
-                    <Route path='*' element={<Navigate to={"/inicio/"}/>}/>
-                    <Route path='/' element={<Presentacion/>}/> 
-                    <Route path='/characters' element={<Characters/>}/>   
-                    <Route path='/characters/:id' element={<CharacterPage />} />
-                    <Route path='/Episode' element={<Episode/>}/>   
-                    <Route path='/Location' element={<Location/>}/>   
-                </Routes>
-        </BrowserRouter>
-    )
-
-}
-
-export default AppRouter
+export default AppRouter;
