267bc75fad331a68476309a761d6eee5f7f6568f
Julian Finelli <julianfinelli97@gmail.com>
2023-12-27 09:20:41 -0300

    chore: sigo probando

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5173',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-    historyApiFallback: {
-      rewrites: [
-        { from: /^\/api/, to: '/api' }, // Redirige las solicitudes a /api al servidor proxy
-        { from: /./, to: '/' }, // Redirige cualquier otra solicitud a la raíz del sitio
-      ],
-    },
-  },
 });
